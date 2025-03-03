15c7aef7a1a7aefff139bdb7a0888fcb6fe1a730
surendrakoppisetti <surendrakoppisetti3@gmail.com>
2025-03-03 20:32:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+ramesh = 1500
+raju = 1000
+venkat = 2000
+abhi = 5000
+rajesh = 500
+fullbill = 10000
+equalbill = fullbill / 5
+
+print("ramesh paid", ramesh - equalbill)
+print("raju paid", raju - equalbill)
+print("venkat paid", venkat - equalbill)
+print("abhi paid", abhi - equalbill)
+print("rajesh paid", rajesh - equalbill)
